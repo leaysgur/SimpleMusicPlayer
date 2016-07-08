5d1af14a562bca6301fcea3de36d795dd750b0d3
leader22 <y.sugiura.0316@gmail.com>
2016-07-08 18:18:07 +0900

    タブ切り替え

--- src/app.js
+++ src/app.js
@@ -2,20 +2,79 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
+  Text,
+  TabBarIOS,
 } from 'react-native';
 
+import {
+  TABS,
+} from './const';
+
 class App extends React.Component {
+  state: Object;
+  _switchTab: () => boolean;
+
+  constructor() {
+    super();
+
+    this.state = {
+      selectedTab: TABS.SONGS
+    };
+    this._switchTab = this._switchTab.bind(this);
+  }
+
+  _switchTab(tabName: string) {
+    this.setState({
+      selectedTab: tabName,
+    });
+  }
+
   render() {
+    const { selectedTab, } = this.state;
+
+    return (
+      <TabBarIOS
+        tintColor="#274a78"
+        unselectedTintColor="#aaa"
+        barTintColor="#eee"
+      >
+        <TabBarIOS.Item
+          title="曲"
+          selected={selectedTab === TABS.SONGS}
+          onPress={ () => { this._switchTab(TABS.SONGS); } }
+        >
+          {this._renderContent(TABS.SONGS)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title="アーティスト"
+          selected={selectedTab === TABS.ARTIST}
+          onPress={ () => { this._switchTab(TABS.ARTIST); } }
+        >
+          {this._renderContent(TABS.ARTIST)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title="アルバム"
+          selected={selectedTab === TABS.ALBUM}
+          onPress={ () => { this._switchTab(TABS.ALBUM); } }
+        >
+          {this._renderContent(TABS.ALBUM)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title="再生中"
+          selected={selectedTab === TABS.PLAYING}
+          onPress={ () => { this._switchTab(TABS.PLAYING); } }
+        >
+          {this._renderContent(TABS.PLAYING)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+
+  _renderContent(pageText: string) {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+        <Text>{pageText}</Text>
       </View>
     );
   }
@@ -27,16 +86,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
---
+++ src/const.js
@@ -0,0 +1,7 @@
+// @flow
+export const TABS = {
+  SONGS:   'SONGS',
+  ALBUM:   'ALBUM',
+  ARTIST:  'ARTIST',
+  PLAYING: 'PLAYING',
+};
