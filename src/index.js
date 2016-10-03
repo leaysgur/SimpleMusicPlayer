6e2cdcf5661d3d85a2dfd53884c472bdbc1c8fc9
leader22 <y.sugiura.0316@gmail.com>
2016-10-03 14:54:23 +0900

    refactor

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ import {
   NativeEventEmitter,
   NativeModules,
 } from 'react-native';
-import {
-  Loading,
-} from './view/common';
+
+import { Loading } from './view/common';
 import App from './app';
+
 import AppStore from './store/app';
 import MediaModel from './model/media';
+
+import NativeAction from './native-action';
+
 const { MediaBridge } = NativeModules;
 
 
-class Bootstrap extends React.Component {
+/**
+ * React Nativeにマウントされるエントリーコンポーネント
+ *
+ * - Native側とのやりとりはココで全てやってしまう
+ *
+ */
+class Index extends React.Component {
   constructor() {
     super();
 
@@ -26,47 +35,8 @@ class Bootstrap extends React.Component {
       }
     }
 
-    this.action = {
-      playSong: (persistentID) => {
-        MediaBridge.playSong(persistentID);
-        AppStore.playingState = 'play';
-      },
-
-      playAlbumSong: (persistentID, albumPersistentID) => {
-        MediaBridge.playAlbumSong(persistentID, albumPersistentID);
-        AppStore.playingState = 'play';
-      },
-
-      togglePlay: () => {
-        MediaBridge.togglePlay().then((state) => {
-          AppStore.playingState = state;
-        });
-      },
-
-      skipNext: () => {
-        MediaBridge.skipNext();
-      },
-
-      skipPrev: () => {
-        MediaBridge.skipPrev();
-      },
-
-      changeRepeat: () => {
-        MediaBridge.changeRepeat().then((mode) => {
-          AppStore.repeatMode = mode;
-        });
-      },
-
-      changeProgress: (val) => {
-        AppStore.seek(val, true);
-        MediaBridge.changePlaybackTime(val);
-      },
-
-      changedProgress: (val) => {
-        MediaBridge.changePlaybackTime(val);
-        AppStore.seek(val, false);
-      },
-    };
+    this.eventEmitter = new NativeEventEmitter(MediaBridge);
+    this.action = new NativeAction(MediaBridge, AppStore);
   }
 
   render() {
@@ -84,22 +54,22 @@ class Bootstrap extends React.Component {
   }
 
   componentDidMount() {
-    const that = this;
-
     MediaBridge.fetchMusic()
     .then((music) => {
       MediaModel.init(music);
 
-      const myEv = new NativeEventEmitter(MediaBridge);
-      myEv.addListener('onPlayItemChanged', (payload) => {
+      this.disposeEmitter = this.eventEmitter.addListener('onPlayItemChanged', (payload) => {
         payload && AppStore.updateNowPlaying(MediaModel.getItem(payload));
       });
 
-      that.forceUpdate();
+      this.forceUpdate();
     });
   }
 
+  componentWillUnmount() {
+    this.disposeEmitter();
+  }
 
 }
 
-export default Bootstrap;
+export default Index;
---
+++ src/native-action.js
@@ -0,0 +1,68 @@
+class NativeAction {
+  constructor(MediaBridge, AppStore) {
+    this.MediaBridge = MediaBridge;
+    this.AppStore    = AppStore;
+
+    const proto = Object.getPrototypeOf(this);
+    Object.getOwnPropertyNames(proto)
+      .filter((name) => {
+        return name !== 'constructor' &&
+          typeof proto[name] === 'function';
+      })
+      .forEach((method) => {
+        this[method] = this[method].bind(this);
+      });
+
+    return this;
+  }
+
+  playSong(persistentID) {
+    const { MediaBridge, AppStore } = this;
+    MediaBridge.playSong(persistentID);
+    AppStore.playingState = 'play';
+  }
+
+  playAlbumSong(persistentID, albumPersistentID) {
+    const { MediaBridge, AppStore } = this;
+    MediaBridge.playAlbumSong(persistentID, albumPersistentID);
+    AppStore.playingState = 'play';
+  }
+
+  togglePlay() {
+    const { MediaBridge, AppStore } = this;
+    MediaBridge.togglePlay().then((state) => {
+      AppStore.playingState = state;
+    });
+  }
+
+  skipNext() {
+    const { MediaBridge } = this;
+    MediaBridge.skipNext();
+  }
+
+  skipPrev() {
+    const { MediaBridge } = this;
+    MediaBridge.skipPrev();
+  }
+
+  changeRepeat() {
+    const { MediaBridge, AppStore } = this;
+    MediaBridge.changeRepeat().then((mode) => {
+      AppStore.repeatMode = mode;
+    });
+  }
+
+  changeProgress(val) {
+    const { MediaBridge, AppStore } = this;
+    AppStore.seek(val, true);
+    MediaBridge.changePlaybackTime(val);
+  }
+
+  changedProgress(val) {
+    const { MediaBridge, AppStore } = this;
+    MediaBridge.changePlaybackTime(val);
+    AppStore.seek(val, false);
+  }
+}
+
+export default NativeAction;
